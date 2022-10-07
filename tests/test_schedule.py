93168886c3cf0021b86385a396ea58d5270b0f00
Naloween <40539093+Naloween@users.noreply.github.com>
2022-10-07 09:41:01 +0200

    feat : tests load schedule

--- tests/test_schedule.py
+++ tests/test_schedule.py
@@ -2,10 +2,30 @@
 from pytest import fixture
 
 from hash_code.Schedule import Schedule
+from hash_code.Task import CreateEmptyBinary
 
 @fixture
 def input_file_name():
     return "tests/input_data_test.txt"
 
-def test_load_schedule(input_file_name):
-    schedule = Schedule.load(input_file_name)
+@fixture
+def schedule(input_file_name):
+    return Schedule.load(input_file_name)
+
+
+def test_load_schedule_sizes(schedule):
+
+    assert schedule.time_limit == 10
+    assert len(schedule.engineers) == 2
+    assert len(schedule.services) == 5
+    assert len(schedule.binaries) == 3
+    assert len(schedule.features) == 2
+    assert CreateEmptyBinary.nb_of_days == 5
+
+def test_load_schedule_content(schedule):
+
+    assert schedule.services[2].name == "sc"
+    assert schedule.features[1].name == "bar"
+    assert schedule.features[1].nb_daily_users == 20
+    assert schedule.features[1].difficulty == 1
+    assert schedule.features[0].services[1].name == "sb"
